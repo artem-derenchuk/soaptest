5495ff034dde3f87619e9d6e46d9b249c16dce05
artem-derenchuk <derenchuk2001@mail.ru>
2021-11-02 09:30:11 +0300

    soap test

---
+++ src/main/java/com/soap/test/SoaptestApplication.java
@@ -0,0 +1,13 @@
+package com.soap.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaptestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaptestApplication.class, args);
+	}
+
+}
